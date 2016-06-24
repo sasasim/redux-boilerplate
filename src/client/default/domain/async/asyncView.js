152b8de1393ad3a0124e15825ce36a7a93c9f164
Tomas Weiss <tomasw@salsitasoft.com>
2016-06-24 10:12:26 +0200

    making a view wrapper

--- src/client/default/domain/async/asyncView.js
+++ src/client/default/domain/async/asyncView.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { view } from 'redux-elm';
 
-import AsyncWrapper from '../../ui/asyncWrapper';
+import ViewWrapper from '../../ui/viewWrapper';
 import Greeting from '../../ui/greeting';
 import Button from '../../ui/button';
 
 export default view(({ model, dispatch }) => (
-  <AsyncWrapper>
+  <ViewWrapper>
     {!model && <Button text="Fetch" onClick={() => dispatch({ type: 'Fetch' })} />}
     {!!model && <Greeting>{model}</Greeting>}
-  </AsyncWrapper>
+  </ViewWrapper>
 ));
--- src/client/default/domain/counter/counterView.js
+++ src/client/default/domain/counter/counterView.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { view } from 'redux-elm';
 
 import Button from '../../ui/button';
+import ViewWrapper from '../../ui/viewWrapper';
 
-export default view(({ model, dispatch }) =>
-  <Button text={`Clicked ${model}x`} onClick={() => dispatch({ type: 'Increment' })} />);
+export default view(({ model, dispatch }) => (
+  <ViewWrapper>
+    <Button text={`Clicked ${model}x`} onClick={() => dispatch({ type: 'Increment' })} />
+  </ViewWrapper>
+));
