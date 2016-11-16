d0ba9d1c9e371155076a09d8d6e806b41a39d0ee
Fredrik Portström <fredrik@salsitasoft.com>
2016-11-16 16:14:16 +0100

    Simplify store.js. Don't compose functions when not necessary.

---
+++ src/store.js
@@ -0,0 +1,31 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import createLogger from 'redux-logger';
+import createSagaMiddleware from 'redux-saga';
+
+import reducer from 'reducers';
+import saga from 'sagas';
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+export default function configureStore() {
+  const sagaMiddleware = createSagaMiddleware();
+  let store;
+  if (isProduction) {
+    store = createStore(reducer, applyMiddleware(sagaMiddleware));
+  } else {
+    const enhancer = applyMiddleware(sagaMiddleware, createLogger());
+    store = createStore(reducer,
+      window.devToolsExtension
+      ? compose(enhancer, window.devToolsExtension())
+      : enhancer
+    );
+  }
+  sagaMiddleware.run(saga);
+  if (!isProduction && module.hot) {
+    module.hot.accept('../reducers', () => {
+      // eslint-disable-next-line global-require
+      store.replaceReducer(require('../reducers').default);
+    });
+  }
+  return store;
+}
