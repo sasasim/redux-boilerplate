5b8910d403f0c229b5256bda773b055ff2e8b6f6
Fredrik Portström <fredrik@salsitasoft.com>
2016-11-16 16:22:00 +0100

    Update path for hot loading after moving file.

--- src/store.js
+++ src/store.js
@@ -24,7 +24,7 @@ export default function configureStore() {
   if (!isProduction && module.hot) {
     module.hot.accept('../reducers', () => {
       // eslint-disable-next-line global-require
-      store.replaceReducer(require('../reducers').default);
+      store.replaceReducer(require('./reducers').default);
     });
   }
   return store;
