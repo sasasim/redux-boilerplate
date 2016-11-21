acf7f6ed21731f58d189c54fed4bda334cc042e5
Tomas Weiss <tomasw@salsitasoft.com>
2016-11-21 17:30:30 +0100

    obey the rules

--- src/constants/actionTypes.js
+++ src/constants/actionTypes.js
@@ -1,10 +1,14 @@
+// Counter
 export const INCREMENT = 'INCREMENT';
 export const DECREMENT = 'DECREMENT';
 
+// Hello User
 export const SAY_HELLO = 'SAY_HELLO';
 export const USER_FETCHED = 'USER_FETCHED';
 
+// Entity Repository
 export const ENTITY_REPOSITORY_HAS_CHANGED = 'ENTITY_REPOSITORY_HAS_CHANGED';
 
+// Routing
 export const ROUTER_INIT = 'ROUTER_INIT';
 export const CHANGE_ROUTE = 'CHANGE_ROUTE';
