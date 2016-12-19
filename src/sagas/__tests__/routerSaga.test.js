ee14f9878122b3dd722652dc738fb4a6778f96e6
Roman Krejcik <farin@farin.cz>
2016-12-19 12:52:02 +0100

    fixed routerSaga test

--- src/sagas/__tests__/routerSaga.test.js
+++ src/sagas/__tests__/routerSaga.test.js
@@ -4,12 +4,16 @@ import { actionTypes } from 'redux-router5';
 import { createMockTask } from 'redux-saga/utils';
 
 const stub = () => 42;
+const stub2 = () => 42;
 const STUB_ROUTE = 'STUB_ROUTE';
+const NESTED_ROUTE = 'NESTED_ROUTE';
+const NESTED_ROUTE_1 = 'NESTED_ROUTE.R1';
 
 describe('Router Saga', () => {
   it('should correctly cancel & start corresponding saga on router transition', () => {
     const it = routerSaga({
-      [STUB_ROUTE]: stub
+      [STUB_ROUTE]: stub,
+      [NESTED_ROUTE]: stub2, //saga declared for parent route
     });
 
     // Wait for transition
@@ -33,12 +37,12 @@ describe('Router Saga', () => {
     expect(it.next({
       payload: {
         route: {
-          name: STUB_ROUTE
+          name: NESTED_ROUTE_1
         }
       }
     }).value).toEqual(cancel(task));
 
-    // Fork the saga again
-    expect(it.next(task).value).toEqual(fork(stub));
+    // Fork the all parent sagas
+    expect(it.next(task).value).toEqual(fork(stub2));
   });
 });
