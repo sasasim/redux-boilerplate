aed39afadca5f7d9294c0a9fd8869d282b29abb6
Fredrik Portström <fredrik@salsitasoft.com>
2017-01-18 11:33:54 +0100

    Router saga: Use transition path.

--- src/sagas/routerSaga.js
+++ src/sagas/routerSaga.js
@@ -1,35 +1,22 @@
-import _ from 'lodash';
-import { call, take, cancel, fork } from 'redux-saga/effects';
-import { startsWithSegment } from 'router5.helpers';
 import { actionTypes } from 'redux-router5';
+import { call, take, cancel, fork } from 'redux-saga/effects';
+import transitionPath from 'router5.transition-path';
 
 import helloUserSaga from 'sagas/helloUserSaga';
 import * as Routes from 'constants/routes';
 
 export function* routerSaga(routingMap) {
-  const sagaTasks = {};
-
+  const routeTasks = {};
   while (true) {
-    const { payload: { route: name } } = yield take(actionTypes.TRANSITION_SUCCESS);
-
-    const matchedRoutes = Object
-      .keys(routingMap)
-      .filter(routeName => startsWithSegment(name, routeName));
-    const activatedRoutes = Object.keys(sagaTasks);
-    const exitedRoutes = _.difference(activatedRoutes, matchedRoutes);
-    const enteredRoutes = _.difference(matchedRoutes, activatedRoutes);
-
-    for (const exited of exitedRoutes) {
-      const saga = sagaTasks[exited];
-      if (saga) {
-        yield cancel(saga);
-        delete sagaTasks[exited];
-      }
+    const { payload: { previousRoute, route } } = yield take(actionTypes.TRANSITION_SUCCESS);
+    const { toActivate, toDeactivate } = transitionPath(route, previousRoute);
+    for (const name of toDeactivate) {
+      yield cancel(routeTasks[name]);
+      delete routeTasks[name];
     }
-
-    for (const entered of enteredRoutes) {
-      const enteredSaga = routingMap[entered];
-      sagaTasks[entered] = yield fork(enteredSaga);
+    for (const name of toActivate) {
+      const enteredSaga = routingMap[name];
+      routeTasks[name] = yield fork(enteredSaga);
     }
   }
 }
--- src/sagas/routerSaga.test.js
+++ src/sagas/routerSaga.test.js
@@ -1,48 +1,148 @@
-import { fork, take, cancel } from 'redux-saga/effects';
-import { routerSaga } from 'sagas/routerSaga';
 import { actionTypes } from 'redux-router5';
+import { fork, take, cancel } from 'redux-saga/effects';
 import { createMockTask } from 'redux-saga/utils';
 
-const stub = () => 42;
-const stub2 = () => 42;
-const STUB_ROUTE = 'STUB_ROUTE';
-const NESTED_ROUTE = 'NESTED_ROUTE';
-const NESTED_ROUTE_1 = 'NESTED_ROUTE.R1';
+import { routerSaga } from 'sagas/routerSaga';
+
+const ROUTE1 = 'ROUTE1';
+const ROUTE11 = 'ROUTE1.1';
+const ROUTE2 = 'ROUTE2';
+const ROUTE21 = 'ROUTE2.1';
+
+const stub1 = () => {};
+const stub11 = () => {};
+const stub2 = () => {};
+const stub21 = () => {};
 
 describe('Router Saga', () => {
-  it('should correctly cancel & start corresponding saga on router transition', () => {
+  const setup = () => {
     const it = routerSaga({
-      [STUB_ROUTE]: stub,
-      [NESTED_ROUTE]: stub2, //saga declared for parent route
+      [ROUTE1]: stub1,
+      [ROUTE11]: stub11,
+      [ROUTE2]: stub2,
+      [ROUTE21]: stub21
     });
-
-    // Wait for transition
     expect(it.next().value).toEqual(take(actionTypes.TRANSITION_SUCCESS));
+    return it;
+  };
 
-    // Fork the matched Saga
+  it('should go to route1 and then route1.1', () => {
+    const it = setup();
     expect(it.next({
       payload: {
         route: {
-          name: STUB_ROUTE
+          name: ROUTE1,
+          meta: { params: {} }
         }
       }
-    }).value).toEqual(fork(stub));
+    }).value).toEqual(fork(stub1));
+    const task1 = createMockTask();
+    expect(it.next(task1).value).toEqual(take(actionTypes.TRANSITION_SUCCESS));
+    expect(it.next({
+      payload: {
+        previousRoute: {
+          name: ROUTE1,
+          meta: { params: {} }
+        },
+        route: {
+          name: ROUTE11,
+          meta: { params: {} }
+        }
+      }
+    }).value).toEqual(fork(stub11));
+    const task11 = createMockTask();
+    expect(it.next(task11).value).toEqual(take(actionTypes.TRANSITION_SUCCESS));
+  });
 
-    const task = createMockTask();
-
-    // Wait for next transition
-    expect(it.next(task).value).toEqual(take(actionTypes.TRANSITION_SUCCESS));
-
-    // Cancel the forked process
+  it('should go to route1.1 and then route1', () => {
+    const it = setup();
     expect(it.next({
       payload: {
         route: {
-          name: NESTED_ROUTE_1
+          name: ROUTE11,
+          meta: { params: {} }
         }
       }
-    }).value).toEqual(cancel(task));
+    }).value).toEqual(fork(stub1));
+    const task1 = createMockTask();
+    expect(it.next(task1).value).toEqual(fork(stub11));
+    const task11 = createMockTask();
+    expect(it.next(task11).value).toEqual(take(actionTypes.TRANSITION_SUCCESS));
+    expect(it.next({
+      payload: {
+        previousRoute: {
+          name: ROUTE11,
+          meta: { params: {} }
+        },
+        route: {
+          name: ROUTE1,
+          meta: { params: {} }
+        }
+      }
+    }).value).toEqual(cancel(task11));
+    expect(it.next().value).toEqual(take(actionTypes.TRANSITION_SUCCESS));
+  });
 
-    // Fork the all parent sagas
-    expect(it.next(task).value).toEqual(fork(stub2));
+  it('should go to route1 and then route2', () => {
+    const it = setup();
+    expect(it.next({
+      payload: {
+        route: {
+          name: ROUTE1,
+          meta: { params: {} }
+        }
+      }
+    }).value).toEqual(fork(stub1));
+    const task1 = createMockTask();
+    expect(it.next(task1).value).toEqual(take(actionTypes.TRANSITION_SUCCESS));
+    expect(it.next({
+      payload: {
+        previousRoute: {
+          name: ROUTE1,
+          meta: { params: {} }
+        },
+        route: {
+          name: ROUTE2,
+          meta: { params: {} }
+        }
+      }
+    }).value).toEqual(cancel(task1));
+    expect(it.next().value).toEqual(fork(stub2));
+    const task2 = createMockTask();
+    expect(it.next(task2).value).toEqual(take(actionTypes.TRANSITION_SUCCESS));
+  });
+
+  it('should go to route1.1 and then route2.1', () => {
+    const it = setup();
+    expect(it.next({
+      payload: {
+        route: {
+          name: ROUTE11,
+          meta: { params: {} }
+        }
+      }
+    }).value).toEqual(fork(stub1));
+    const task1 = createMockTask();
+    expect(it.next(task1).value).toEqual(fork(stub11));
+    const task11 = createMockTask();
+    expect(it.next(task11).value).toEqual(take(actionTypes.TRANSITION_SUCCESS));
+    expect(it.next({
+      payload: {
+        previousRoute: {
+          name: ROUTE11,
+          meta: { params: {} }
+        },
+        route: {
+          name: ROUTE21,
+          meta: { params: {} }
+        }
+      }
+    }).value).toEqual(cancel(task11));
+    expect(it.next().value).toEqual(cancel(task1));
+    expect(it.next().value).toEqual(fork(stub2));
+    const task2 = createMockTask();
+    expect(it.next(task2).value).toEqual(fork(stub21));
+    const task21 = createMockTask();
+    expect(it.next(task21).value).toEqual(take(actionTypes.TRANSITION_SUCCESS));
   });
 });
